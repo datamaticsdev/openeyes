3c108e93b9a2ebb6bebdadeb5338e06d65a7a88e
Tamas Vedelek <vetusko@gmail.com>
2016-06-10 10:19:00 +0100

    - fixed doctor_grade_id data type

--- protected/migrations/m150505_113733_add_grade_reg_code.php
+++ protected/migrations/m150505_113733_add_grade_reg_code.php
@@ -4,8 +4,8 @@ class m150505_113733_add_grade_reg_code extends OEMigration
 {
 	public function up()
 	{
-		$this->addColumn('user', 'doctor_grade_id', 'int(3) null default null');
-		$this->addColumn('user_version', 'doctor_grade_id', 'int(3) null default null');
+		$this->addColumn('user', 'doctor_grade_id', 'int(11) null default null');
+		$this->addColumn('user_version', 'doctor_grade_id', 'int(11) null default null');
 
 		$this->addForeignKey('user_doctor_grade_id_fk','user','doctor_grade_id','doctor_grade','id');
 
