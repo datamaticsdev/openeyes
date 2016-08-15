1d73da2edf198ec454321fb389e45da8eaef538d
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-08-15 12:56:29 +0100

    + update favicon

--- protected/views/base/head/_meta.php
+++ protected/views/base/head/_meta.php
@@ -14,8 +14,14 @@
 	<meta http-equiv="pragma" content="no-cache" />
 <?php }?>
 
-<link rel="icon" href="<?php echo Yii::app()->createUrl('favicon.ico')?>" type="image/x-icon" />
-<link rel="shortcut icon" href="<?php echo Yii::app()->createUrl('favicon.ico')?>"/>
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::app()->createUrl('/apple-touch-icon.png?v=9Bayz8PxAO') ?>">
+<link rel="icon" type="image/png" href="<?= Yii::app()->createUrl('/favicon-32x32.png?v=9Bayz8PxAO');?>" sizes="32x32">
+<link rel="icon" type="image/png" href="<?= Yii::app()->createUrl('/favicon-16x16.png?v=9Bayz8PxAO');?>" sizes="16x16">
+<link rel="manifest" href="<?= Yii::app()->createUrl('/manifest.json?v=9Bayz8PxAO');?>">
+<link rel="mask-icon" href="<?= Yii::app()->createUrl('/safari-pinned-tab.svg?v=9Bayz8PxAO');?>" color="#3e4edb">
+<link rel="shortcut icon" href="<?= Yii::app()->createUrl('/favicon.ico?v=9Bayz8PxAO');?>">
+<meta name="theme-color" content="#ffffff">
 
 <script type="text/javascript">
 	var baseUrl = '<?php echo Yii::app()->baseUrl?>';
